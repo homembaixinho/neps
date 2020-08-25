4deaf79569fdca22afaf5d99dd4d64813c6e8ea8
heitor <heitorleite.dev@gmail.com>
2020-08-25 09:19:27 -0300

    memozation

--- tecnicas/papa.cpp
+++ tecnicas/papa.cpp
@@ -8,26 +8,27 @@
 
 #define MAXN 1001
 #define MAXR 1000001
-using namespace std;
 
-int n;
+using namespace std;
+using par = pair<int, int>;
+using memo = map<par, int>;
+
 struct caixa { int p, r; };
 bool operator <(caixa a, caixa b) { return a.r < b.r; }
 
+int n;
 caixa cx[MAXN];
-map<pair<int, int>, int> memo;
-map<pair<int, int>, int>::iterator it;
-pair<int, int> k;
+memo dp;
 
 int knapsack(int n, int c) {
   if (n == 0 || c == 0 || cx[n].r < 0) return 0;
   if (cx[n].p > c) return knapsack(n-1, c);
 
-  k = make_pair(n, c);
-  it = memo.find(k);
+  par k = make_pair(n, c);
+  memo::iterator it = dp.find(k);
 
-  if (it == memo.end())
-    return memo[k] = max(
+  if (it == dp.end())
+    return dp[k] = max(
       knapsack(n-1, c),
       1 + knapsack(n-1, min(c-cx[n].p, cx[n].r))
     );
